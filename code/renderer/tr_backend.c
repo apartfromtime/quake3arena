a0a4ed34d5af131d146aa8f284a8e8ba3aa6cfab
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2023-07-13 22:39:26 +1000

    Wide aspect ratio screens need to have the sides cleared unless they are displaying game renderings.
    See SCR_DrawScreenField(). This is never finalised. RB_EndSurface produces these these elements and draws them to screen, otherwise no drawing is caused.
    
    Thanks to ioQuake3 for direction on fixing this issue.

--- code/renderer/tr_backend.c
+++ code/renderer/tr_backend.c
@@ -728,6 +728,11 @@ void RE_StretchRaw (int x, int y, int w, int h, int cols, int rows, const byte *
 	}
 	R_SyncRenderThread();
 
+	if (tess.numIndexes)
+	{
+		RB_EndSurface();
+	}
+
 	// we definately want to sync every frame for the cinematics
 	qglFinish();
 
