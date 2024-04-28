0b6795c533b180dec65c8c76506b543a45a039fd
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2024-04-28 17:53:07 +1000

    Remove virtual machine bytecode interpreter
    Remove virtual machine bytecode interpreter.
    Remove "vm_x86.c" and "vm_interpreted.c".

--- code/client/cl_ui.c
+++ code/client/cl_ui.c
@@ -1144,17 +1144,9 @@ CL_InitUI
 
 void CL_InitUI( void ) {
 	int		v;
-	vmInterpret_t		interpret;
 
-	// load the dll or bytecode
-	if ( cl_connectedToPureServer != 0 ) {
-		// if sv_pure is set we only allow qvms to be loaded
-		interpret = VMI_COMPILED;
-	}
-	else {
-		interpret = Cvar_VariableValue( "vm_ui" );
-	}
-	uivm = VM_Create( "ui", CL_UISystemCalls, interpret );
+	// load the dll
+	uivm = VM_Create("ui", CL_UISystemCalls);
 	if ( !uivm ) {
 		Com_Error( ERR_FATAL, "VM_Create on UI failed" );
 	}
--- code/qcommon/qcommon.h
+++ code/qcommon/qcommon.h
@@ -286,12 +286,6 @@ VIRTUAL MACHINE
 typedef struct vm_s vm_t;
 
 typedef enum {
-	VMI_NATIVE,
-	VMI_BYTECODE,
-	VMI_COMPILED
-} vmInterpret_t;
-
-typedef enum {
 	TRAP_MEMSET = 100,
 	TRAP_MEMCPY,
 	TRAP_STRNCPY,
@@ -309,21 +303,16 @@ typedef enum {
 	TRAP_TESTPRINTFLOAT
 } sharedTraps_t;
 
-void	VM_Init( void );
-vm_t	*VM_Create( const char *module, int (*systemCalls)(int *), 
-				   vmInterpret_t interpret );
+void VM_Init(void);
+vm_t* VM_Create(const char *module, int (*systemCalls)(int *));
 // module should be bare: "cgame", not "cgame.dll" or "vm/cgame.qvm"
-
-void	VM_Free( vm_t *vm );
-void	VM_Clear(void);
-vm_t	*VM_Restart( vm_t *vm );
-
-int		Q_CDECL VM_Call( vm_t *vm, int callNum, ... );
-
-void	VM_Debug( int level );
-
-void	*VM_ArgPtr( int intValue );
-void	*VM_ExplicitArgPtr( vm_t *vm, int intValue );
+void VM_Free(vm_t* vm);
+void VM_Clear(void);
+vm_t* VM_Restart(vm_t* vm);
+int Q_CDECL VM_Call(vm_t* vm, int callNum, ...);
+void VM_Debug(int level);
+void* VM_ArgPtr(int intValue);
+void* VM_ExplicitArgPtr(vm_t* vm, int intValue);
 
 /*
 ==============================================================
