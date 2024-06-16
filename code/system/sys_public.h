adac1ad5f2eca208be2b9a5a394430ac9b5cd32a
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2024-06-16 12:57:19 +1000

    Removed unreferenced pragma warnings

--- code/system/sys_public.h
+++ code/system/sys_public.h
@@ -3,27 +3,8 @@
 
 #ifdef _WIN32
 #pragma warning(disable : 4018)			// signed/unsigned mismatch
-#pragma warning(disable : 4032)			// formal parameter 'number' has different type when promoted
-#pragma warning(disable : 4051)
-#pragma warning(disable : 4057)			// slightly different base types
-#pragma warning(disable : 4100)			// unreferenced formal parameter
-#pragma warning(disable : 4115)
-#pragma warning(disable : 4125)			// decimal digit terminates octal escape sequence
-#pragma warning(disable : 4127)			// conditional expression is constant
-#pragma warning(disable : 4136)
-#pragma warning(disable : 4152)			// nonstandard extension, function/data pointer conversion in expression
-#pragma warning(disable : 4201)			// nonstandard extension used : nameless struct/union
-#pragma warning(disable : 4214)			// nonstandard extension used : bit field types other than int
-#pragma warning(disable : 4220)			// varargs matches remaining parameters
 #pragma warning(disable : 4244)			// 'conversion' conversion from 'type1' to 'type2', possible loss of data
 #pragma warning(disable : 4142)			// benign redefinition
-#pragma warning(disable : 4305)			// truncation from const double to float
-#pragma warning(disable : 4310)			// cast truncates constant value
-#pragma warning(disable:  4505)			// unreferenced local function has been removed
-#pragma warning(disable : 4514)			// unreferenced inline function has been removed
-#pragma warning(disable : 4702)			// unreachable code
-#pragma warning(disable : 4710)			// function 'blah' not inlined
-#pragma warning(disable : 4711)			// selected for automatic inline expansion
 #pragma warning(disable : 4996)			// This function or variable may be unsafe
 #endif
 
