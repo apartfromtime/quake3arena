8749ab69f1a6a89227ba40af0dc52693619f0b07
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2024-09-01 11:34:08 +1000

    client: Client type definitions

--- code/client/cl_types.h
+++ code/client/cl_types.h
@@ -20,8 +20,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-#ifndef Q_CLIENT_TYPES_H
-#define Q_CLIENT_TYPES_H
+#ifndef Q_CL_TYPES_H
+#define Q_CL_TYPES_H
 
 /*
 =============================================================================
@@ -34,10 +34,24 @@ CLIENT
 // print levels from renderer (FIXME: set up for game / cgame?)
 typedef enum
 {
-	PRINT_ALL,
-	PRINT_DEVELOPER,		// only print when "developer 1"
-	PRINT_WARNING,
-	PRINT_ERROR
+    PRINT_ALL,
+    PRINT_DEVELOPER,        // only print when "developer 1"
+    PRINT_WARNING,
+    PRINT_ERROR
 } printParm_t;
 
-#endif // #ifndef Q_CLIENT_TYPES_H
+typedef enum
+{
+    CA_UNINITIALIZED,
+    CA_DISCONNECTED,        // not talking to a server
+    CA_AUTHORIZING,         // not used any more, was checking cd key 
+    CA_CONNECTING,          // sending request packets to the server
+    CA_CHALLENGING,         // sending challenge packets to the server
+    CA_CONNECTED,           // netchan_t established, getting gamestate
+    CA_LOADING,             // only during cgame initialization, never during main loop
+    CA_PRIMED,              // got gamestate, waiting for first frame
+    CA_ACTIVE,              // game views should be displayed
+    CA_CINEMATIC            // playing a cinematic or a static pic, not connected to a server
+} connstate_t;
+
+#endif // #ifndef Q_CL_TYPES_H
