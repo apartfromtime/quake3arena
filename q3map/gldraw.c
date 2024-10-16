fce63af944c5e57e10d9dc887dfdc49d26407d53
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2024-10-16 13:10:37 +1100

    map: Replace glaux with glut (#7)
    Replace GL Auxiliary Library with GL Utility support.
    Removed unused configurations.

--- q3map/gldraw.c
+++ q3map/gldraw.c
@@ -23,7 +23,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <windows.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
-#include <GL/glaux.h>
+#include <GL/glut.h>
 
 #include "qbsp.h"
 
@@ -38,9 +38,10 @@ vec3_t	draw_mins, draw_maxs;
 
 void InitWindow (void)
 {
-    auxInitDisplayMode (AUX_SINGLE | AUX_RGB);
-    auxInitPosition (0, 0, WIN_SIZE, WIN_SIZE);
-    auxInitWindow ("qcsg");
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition (GLUT_WINDOW_X, GLUT_WINDOW_Y);
+    glutInitWindowSize(GLUT_WINDOW_WIDTH, GLUT_WINDOW_HEIGHT);
+    glutCreateWindow ("qcsg");
 }
 
 void Draw_ClearWindow (void)
