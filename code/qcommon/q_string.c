e6affcf06cbce4107f01220a0084440e2948e123
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2024-11-03 14:44:09 +1100

    Moved filter path
    Moved filter path from common to string.
    Removed unused code.

--- code/qcommon/q_string.c
+++ code/qcommon/q_string.c
@@ -401,6 +401,38 @@ int Com_Filter(const char* filter, const char* name, int casesensitive)
 }
 
 /*
+============
+Com_FilterPath
+============
+*/
+int Com_FilterPath(char* filter, char* name, int casesensitive)
+{
+    int i;
+    char new_filter[MAX_QPATH];
+    char new_name[MAX_QPATH];
+
+    for (i = 0; i < MAX_QPATH - 1 && filter[i]; i++) {
+        if (filter[i] == '\\' || filter[i] == ':') {
+            new_filter[i] = '/';
+        }
+        else {
+            new_filter[i] = filter[i];
+        }
+    }
+    new_filter[i] = '\0';
+    for (i = 0; i < MAX_QPATH - 1 && name[i]; i++) {
+        if (name[i] == '\\' || name[i] == ':') {
+            new_name[i] = '/';
+        }
+        else {
+            new_name[i] = name[i];
+        }
+    }
+    new_name[i] = '\0';
+    return Com_Filter(new_filter, new_name, casesensitive);
+}
+
+/*
 ================
 Com_HashString
 
