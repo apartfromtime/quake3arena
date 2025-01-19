aabdc4e8dcab41ca21c6cfe81fe282fbd692295a
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2025-01-20 09:36:57 +1100

    memory: Update comments

--- code/qcommon/q_heap.c
+++ code/qcommon/q_heap.c
@@ -37,15 +37,8 @@ extern qhandle_t logfile;
 /*
 ==============================================================================
 
-						ZONE MEMORY ALLOCATION
+ZONE MEMORY ALLOCATION
 
-There is never any space between memblocks, and there will never be two
-contiguous free memblocks.
-
-The rover can be left pointing at a non-empty block
-
-The zone calls are pretty much only used for small strings and structures,
-all big things are allocated on the hunk.
 ==============================================================================
 */
 
@@ -304,33 +297,7 @@ char* CopyString(const char* in) {
 /*
 ==============================================================================
 
-Goals:
-	reproducable without history effects -- no out of memory errors on weird map to map changes
-	allow restarting of the client without fragmentation
-	minimize total pages in use at run time
-	minimize total pages needed during load time
-
-  Single block of memory with stack allocators coming from both ends towards the middle.
-
-  One side is designated the temporary memory allocator.
-
-  Temporary memory can be allocated and freed in any order.
-
-  A highwater mark is kept of the most in use at any time.
-
-  When there is no temporary memory allocated, the permanent and temp sides
-  can be switched, allowing the already touched temp memory to be used for
-  permanent storage.
-
-  Temp memory must never be allocated on two ends at once, or fragmentation
-  could occur.
-
-  If we have any in-use temp memory, additional temp allocations must come from
-  that side.
-
-  If not, we can choose to make either side the new temp side and push future
-  permanent allocations to the other side.  Permanent allocations should be
-  kept on the side that has the current greatest wasted highwater mark.
+HUNK MEMORY ALLOCATION
 
 ==============================================================================
 */
