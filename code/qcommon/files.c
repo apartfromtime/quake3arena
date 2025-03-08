2d316cd0ca635188c2e0b470407c8c094f7bb55e
apartfromtime <42292382+apartfromtime@users.noreply.github.com>
2025-03-08 14:48:13 +1100

    common: Made unzip external
    Moved unzip to external libraries.

--- code/qcommon/files.c
+++ code/qcommon/files.c
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "../game/q_shared.h"
 #include "qcommon.h"
-#include "unzip.h"
+#include "../../libs/minizip/unzip.h"
 
 /*
 =============================================================================
